1e1992d239ec36e13f08f07b93dd7bf6d8ff73e2
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-03-16 15:41:38 +1100

    Updated theme manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,11 +25,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `myweekinjs`,
+        short_name: `myweekinjs`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#BAF2D8`,
+        theme_color: `#BAF2D8`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
