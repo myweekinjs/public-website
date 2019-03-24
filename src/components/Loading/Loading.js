b952b1f229723cef16501ce06acd30f2d3aafea6
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-03-24 16:48:09 +1100

    Minor syntax fix

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,7 +3,7 @@ import './loading.scss'
 
 const Loading = () => (
   <div className="loader loader--style2" title="1">
-    <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" style={{ "enableBackground" : "new 0 0 50 50;" }}>
+    <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" style={{ "enableBackground" : "new 0 0 50 50" }}>
       <path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
         <animateTransform attributeType="xml"
           attributeName="transform"
